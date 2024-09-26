32f59701ea656a8b9f77ac6c0cad8d31577b1c05
Sander W. van der Laan <s.w.vanderlaan@gmail.com>
2024-09-26 14:54:35 -0400

    fix

--- src/variant_ref_create.sh
+++ src/variant_ref_create.sh
@@ -151,88 +151,88 @@ echo "  Verbosity.................: $VERBOSE"
 echo "  Version...................: $VERSION ($VERSION_DATE)"
 echo ""
 
-# # Submit a job to download data for chromosomes 1-22 and X
-# if [[ $VERBOSE -eq 1 ]]; then
-#     echo "> Submitting job to download the phased high-coverage 1000 Genomes VCF files."
-# fi
-# SLURM_DOWNLOAD=$(sbatch --array=1-23 --job-name=pp_download_vcf --output="$RESOURCE_DIR/pp_download_vcf_%A_%a.out" --error="$RESOURCE_DIR/pp_download_vcf_%A_%a.err" --ntasks=1 --cpus-per-task=1 --mem=8G --time=00:30:00 --mail-type="$SBATCH_MAIL" --mail-user="$SBATCH_MAIL_USER" << EOF
-# #!/bin/bash
-# CHR=\${SLURM_ARRAY_TASK_ID}
-# if [[ "\$CHR" == "23" ]]; then
-#     wget -P "$RESOURCE_DIR" http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/data_collections/1000G_2504_high_coverage/working/20220422_3202_phased_SNV_INDEL_SV/1kGP_high_coverage_Illumina.chrX.filtered.SNV_INDEL_SV_phased_panel.v2.vcf.gz
-# else
-#     wget -P "$RESOURCE_DIR" http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/data_collections/1000G_2504_high_coverage/working/20220422_3202_phased_SNV_INDEL_SV/1kGP_high_coverage_Illumina.chr\${CHR}.filtered.SNV_INDEL_SV_phased_panel.vcf.gz
-# fi
-# EOF
-# )
+# Submit a job to download data for chromosomes 1-22 and X
+if [[ $VERBOSE -eq 1 ]]; then
+    echo "> Submitting job to download the phased high-coverage 1000 Genomes VCF files."
+fi
+SLURM_DOWNLOAD=$(sbatch --array=1-23 --job-name=pp_download_vcf --output="$RESOURCE_DIR/pp_download_vcf_%A_%a.out" --error="$RESOURCE_DIR/pp_download_vcf_%A_%a.err" --ntasks=1 --cpus-per-task=1 --mem=8G --time=00:30:00 --mail-type="$SBATCH_MAIL" --mail-user="$SBATCH_MAIL_USER" << EOF
+#!/bin/bash
+CHR=\${SLURM_ARRAY_TASK_ID}
+if [[ "\$CHR" == "23" ]]; then
+    wget -P "$RESOURCE_DIR" http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/data_collections/1000G_2504_high_coverage/working/20220422_3202_phased_SNV_INDEL_SV/1kGP_high_coverage_Illumina.chrX.filtered.SNV_INDEL_SV_phased_panel.v2.vcf.gz
+else
+    wget -P "$RESOURCE_DIR" http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/data_collections/1000G_2504_high_coverage/working/20220422_3202_phased_SNV_INDEL_SV/1kGP_high_coverage_Illumina.chr\${CHR}.filtered.SNV_INDEL_SV_phased_panel.vcf.gz
+fi
+EOF
+)
 
-# # Extract the job ID from the output
-# SLURM_DOWNLOAD_JOBID=$(echo $SLURM_DOWNLOAD | awk '{print $4}')
+# Extract the job ID from the output
+SLURM_DOWNLOAD_JOBID=$(echo $SLURM_DOWNLOAD | awk '{print $4}')
 
-# # Submit array job to filter chromosomes 1-22 and X
-# if [[ $VERBOSE -eq 1 ]]; then
-#     echo "> Submitting job to filter the VCF files."
-# fi
-# # SLURM_CREATE=$(sbatch --array=1-23 --job-name=pp_create --output="$RESOURCE_DIR/pp_create_%A_%a.out" --error="$RESOURCE_DIR/pp_create_%A_%a.err" --ntasks=1 --cpus-per-task=1 --mem=8G --time=01:00:00 --mail-type="$SBATCH_MAIL" --mail-user="$SBATCH_MAIL_USER" << EOF
-# SLURM_CREATE=$(sbatch --dependency=afterok:$SLURM_DOWNLOAD_JOBID --array=1-23 --job-name=pp_create --output="$RESOURCE_DIR/pp_create_%A_%a.out" --error="$RESOURCE_DIR/pp_create_%A_%a.err" --ntasks=1 --cpus-per-task=1 --mem=8G --time=00:30:00 --mail-type="$SBATCH_MAIL" --mail-user="$SBATCH_MAIL_USER" << EOF
-# #!/bin/bash
-# source ~/.bashrc
-# mamba activate monopogen
-# CHROM=\${SLURM_ARRAY_TASK_ID}
+# Submit array job to filter chromosomes 1-22 and X
+if [[ $VERBOSE -eq 1 ]]; then
+    echo "> Submitting job to filter the VCF files."
+fi
+# SLURM_CREATE=$(sbatch --array=1-23 --job-name=pp_create --output="$RESOURCE_DIR/pp_create_%A_%a.out" --error="$RESOURCE_DIR/pp_create_%A_%a.err" --ntasks=1 --cpus-per-task=1 --mem=8G --time=01:00:00 --mail-type="$SBATCH_MAIL" --mail-user="$SBATCH_MAIL_USER" << EOF
+SLURM_CREATE=$(sbatch --dependency=afterok:$SLURM_DOWNLOAD_JOBID --array=1-23 --job-name=pp_create --output="$RESOURCE_DIR/pp_create_%A_%a.out" --error="$RESOURCE_DIR/pp_create_%A_%a.err" --ntasks=1 --cpus-per-task=1 --mem=8G --time=00:30:00 --mail-type="$SBATCH_MAIL" --mail-user="$SBATCH_MAIL_USER" << EOF
+#!/bin/bash
+source ~/.bashrc
+mamba activate monopogen
+CHROM=\${SLURM_ARRAY_TASK_ID}
 
-# if [[ "\$CHROM" == "23" ]]; then
-#     echo "Processing chromosome X"
-#     VCF_IN="${RESOURCE_DIR}/1kGP_high_coverage_Illumina.chrX.filtered.SNV_INDEL_SV_phased_panel.v2.vcf.gz"
-#     OUT_FILE="${OUT_DIR}/1kGP_high_coverage_Illumina.SNVonly_poly.filtered_${AF_FIELD}_${AF_SCI}.chrX.vcf.gz"
+if [[ "\$CHROM" == "23" ]]; then
+    echo "Processing chromosome X"
+    VCF_IN="${RESOURCE_DIR}/1kGP_high_coverage_Illumina.chrX.filtered.SNV_INDEL_SV_phased_panel.v2.vcf.gz"
+    OUT_FILE="${OUT_DIR}/1kGP_high_coverage_Illumina.SNVonly_poly.filtered_${AF_FIELD}_${AF_SCI}.chrX.vcf.gz"
 
-#     echo "> Indexing \$VCF_IN" 
-#     tabix -fp vcf \$VCF_IN
+    echo "> Indexing \$VCF_IN" 
+    tabix -fp vcf \$VCF_IN
 
-#     echo "> Applying filtering and bi-allelic SNP selection"
-#     # Step 1: Ensure AF tag is present
-#     bcftools +fill-tags \$VCF_IN --output-type z -o ${OUT_DIR}/temp.chrX.vcf.gz -- -t AF,INFO/TYPE
-#     tabix -fp vcf ${OUT_DIR}/temp.chrX.vcf.gz
+    echo "> Applying filtering and bi-allelic SNP selection"
+    # Step 1: Ensure AF tag is present
+    bcftools +fill-tags \$VCF_IN --output-type z -o ${OUT_DIR}/temp.chrX.vcf.gz -- -t AF,INFO/TYPE
+    tabix -fp vcf ${OUT_DIR}/temp.chrX.vcf.gz
 
-#     # Step 2: Apply filtering and bi-allelic SNP selection
-#     bcftools view --include '$AF_FIELD>$AF & TYPE="$VARIANT_TYPE"' -m2 -M2 --types snps --regions chrX --output-type z -o \$OUT_FILE ${OUT_DIR}/temp.chrX.vcf.gz 
-#     rm -v ${OUT_DIR}/temp.chrX.vcf.gz
-#     rm -v ${OUT_DIR}/temp.chrX.vcf.gz.tbi
+    # Step 2: Apply filtering and bi-allelic SNP selection
+    bcftools view --include '$AF_FIELD>$AF & TYPE="$VARIANT_TYPE"' -m2 -M2 --types snps --regions chrX --output-type z -o \$OUT_FILE ${OUT_DIR}/temp.chrX.vcf.gz 
+    rm -v ${OUT_DIR}/temp.chrX.vcf.gz
+    rm -v ${OUT_DIR}/temp.chrX.vcf.gz.tbi
     
-#     echo "> Indexing \$OUT_FILE"
-#     tabix -fp vcf \$OUT_FILE
-# else
-#     echo "Processing chromosome \$CHROM"
-#     VCF_IN="${RESOURCE_DIR}/1kGP_high_coverage_Illumina.chr\${CHROM}.filtered.SNV_INDEL_SV_phased_panel.vcf.gz"
-#     OUT_FILE="${OUT_DIR}/1kGP_high_coverage_Illumina.SNVonly_poly.filtered_${AF_FIELD}_${AF_SCI}.chr\${CHROM}.vcf.gz"
+    echo "> Indexing \$OUT_FILE"
+    tabix -fp vcf \$OUT_FILE
+else
+    echo "Processing chromosome \$CHROM"
+    VCF_IN="${RESOURCE_DIR}/1kGP_high_coverage_Illumina.chr\${CHROM}.filtered.SNV_INDEL_SV_phased_panel.vcf.gz"
+    OUT_FILE="${OUT_DIR}/1kGP_high_coverage_Illumina.SNVonly_poly.filtered_${AF_FIELD}_${AF_SCI}.chr\${CHROM}.vcf.gz"
 
-#     echo "> Indexing \$VCF_IN"
-#     tabix -fp vcf \$VCF_IN
+    echo "> Indexing \$VCF_IN"
+    tabix -fp vcf \$VCF_IN
 
-#     echo "> Applying filtering and bi-allelic SNP selection"
-#     # Step 1: Ensure AF tag is present
-#     bcftools +fill-tags \$VCF_IN --output-type z -o ${OUT_DIR}/temp.chr\${CHROM}.vcf.gz -- -t AF,INFO/TYPE
-#     tabix -fp vcf ${OUT_DIR}/temp.chr\${CHROM}.vcf.gz
+    echo "> Applying filtering and bi-allelic SNP selection"
+    # Step 1: Ensure AF tag is present
+    bcftools +fill-tags \$VCF_IN --output-type z -o ${OUT_DIR}/temp.chr\${CHROM}.vcf.gz -- -t AF,INFO/TYPE
+    tabix -fp vcf ${OUT_DIR}/temp.chr\${CHROM}.vcf.gz
 
-#     # Step 2: Apply filtering and bi-allelic SNP selection
-#     bcftools view --include '$AF_FIELD>$AF & TYPE="$VARIANT_TYPE"' -m2 -M2 --types snps --regions chr\$CHROM --output-type z -o \$OUT_FILE ${OUT_DIR}/temp.chr\${CHROM}.vcf.gz 
-#     rm -v ${OUT_DIR}/temp.chr\${CHROM}.vcf.gz
-#     rm -v ${OUT_DIR}/temp.chr\${CHROM}.vcf.gz.tbi
+    # Step 2: Apply filtering and bi-allelic SNP selection
+    bcftools view --include '$AF_FIELD>$AF & TYPE="$VARIANT_TYPE"' -m2 -M2 --types snps --regions chr\$CHROM --output-type z -o \$OUT_FILE ${OUT_DIR}/temp.chr\${CHROM}.vcf.gz 
+    rm -v ${OUT_DIR}/temp.chr\${CHROM}.vcf.gz
+    rm -v ${OUT_DIR}/temp.chr\${CHROM}.vcf.gz.tbi
 
-#     echo "> Indexing \$OUT_FILE"
-#     tabix -fp vcf \$OUT_FILE
-# fi
-# EOF
-# )
+    echo "> Indexing \$OUT_FILE"
+    tabix -fp vcf \$OUT_FILE
+fi
+EOF
+)
 
-# # Extract the job ID from the output
-# SLURM_CREATE_JOBID=$(echo $SLURM_CREATE | awk '{print $4}')
+# Extract the job ID from the output
+SLURM_CREATE_JOBID=$(echo $SLURM_CREATE | awk '{print $4}')
 
 # Wait for the array job to finish before normalizing the files
 if [[ $VERBOSE -eq 1 ]]; then
     echo "> Submitting job to normalize the filtered VCF files."
 fi
-# SLURM_NORM=$(sbatch --dependency=afterok:$SLURM_CREATE_JOBID --array=1-23 --job-name=pp_norm --output="$RESOURCE_DIR/pp_norm_%A_%a.out" --error="$RESOURCE_DIR/pp_norm_%A_%a.err" --ntasks=1 --cpus-per-task=1 --mem=8G --time=01:00:00 --mail-type="$SBATCH_MAIL" --mail-user="$SBATCH_MAIL_USER" << EOF
-SLURM_NORM=$(sbatch --array=1-23 --job-name=pp_norm --output="$RESOURCE_DIR/pp_norm_%A_%a.out" --error="$RESOURCE_DIR/pp_norm_%A_%a.err" --ntasks=1 --cpus-per-task=1 --mem=8G --time=01:00:00 --mail-type="$SBATCH_MAIL" --mail-user="$SBATCH_MAIL_USER" << EOF
+# SLURM_NORM=$(sbatch --array=1-23 --job-name=pp_norm --output="$RESOURCE_DIR/pp_norm_%A_%a.out" --error="$RESOURCE_DIR/pp_norm_%A_%a.err" --ntasks=1 --cpus-per-task=1 --mem=8G --time=01:00:00 --mail-type="$SBATCH_MAIL" --mail-user="$SBATCH_MAIL_USER" << EOF
+SLURM_NORM=$(sbatch --dependency=afterok:$SLURM_CREATE_JOBID --array=1-23 --job-name=pp_norm --output="$RESOURCE_DIR/pp_norm_%A_%a.out" --error="$RESOURCE_DIR/pp_norm_%A_%a.err" --ntasks=1 --cpus-per-task=1 --mem=8G --time=01:00:00 --mail-type="$SBATCH_MAIL" --mail-user="$SBATCH_MAIL_USER" << EOF
 #!/bin/bash
 source ~/.bashrc
 mamba activate monopogen
