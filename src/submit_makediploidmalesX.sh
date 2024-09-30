a37fe310eec1529e2b04f02fa9db19859513d7f7
Sander W. van der Laan <s.w.vanderlaan@gmail.com>
2024-09-30 12:49:12 -0400

    fix

--- src/submit_makediploidmalesX.sh
+++ src/submit_makediploidmalesX.sh
@@ -312,6 +312,7 @@ fi
 
 # Extract the region from the BED file
 RAW_REGION="$(sed -n \$CHUNK_NUMBER \$BED_FILE)"
+sed -n \$CHUNK_NUMBER \$BED_FILE
 if [[ \$DEBUG_FLAG -eq "$DEBUG" ]]; then
     echo "DEBUG: Extracted raw region: \$RAW_REGION (chunk number: \$CHUNK_NUMBER of $CHUNK_SIZE)"
 fi
