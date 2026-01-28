b668f05f60eb62118ef69f5f320d29de6aaeaf28
Sander W. van der Laan <swvanderlaan@users.noreply.github.com>
2026-01-28 16:32:47 +0100

    updates to example

--- example/runPreprocess_submit_jobs.sh
+++ example/runPreprocess_submit_jobs.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#SBATCH --job-name=submit_mpg_jobs
+#SBATCH --output=/sfs/gpfs/tardis/project/cphg-millerlab/swvanderlaan_man7zh/MetaPlaq/monopogen/submit_mpg_%j.out
+#SBATCH --error=/sfs/gpfs/tardis/project/cphg-millerlab/swvanderlaan_man7zh/MetaPlaq/monopogen/submit_mpg_%j.err
+#SBATCH --ntasks=1
+#SBATCH --time=00:10:00
+#SBATCH --partition=standard
+#SBATCH -A cphg-millerlab
 
 # Description: 
 # Submit preProcess of Monopogen on the provided BAM files.
@@ -7,16 +14,29 @@
 # provided in the SLURM header.
 # 
 # Change log:
-# Version: 1.2.6
+# Version: 1.2.2
 # Author: Sander W. van der Laan
-# Date: 2025-06-05
-# Usage: bash runPreprocess_submit_jobs.sh
+# Date: 2026-01-28
+# Usage: bash submit_jobs_runPreprocess_rivanna.sh
 
 # Define base directory where studies are stored
-STUDY_DIR="/sfs/gpfs/tardis/project/cphg-millerlab/MetaPlaq/v2/raw_data/post_alignment_outs/cellranger_outs"
+# CELLRANGER
+# STUDY_DIR="/sfs/gpfs/tardis/project/cphg-millerlab/MetaPlaq/v2/raw_data/post_alignment_outs/cellranger_outs"
+# STARSOLO
+STUDY_DIR="/sfs/gpfs/tardis/project/cphg-millerlab/MetaPlaq/v2/raw_data/post_alignment_outs/starsolo_outs"
 
 # List of available studies (can be manually updated if needed)
-STUDIES=("YOUR_STUDY_1" "YOUR_STUDY_2" "YOUR_STUDY_3")
+# CELLRANGER
+# STUDIES=("Alsaigh_et_al_2022" "Bashore_et_al_2024" "Cheng_et_al" "Chou_et_al_2021" "Eberhardt_et_al_2023" "Fernandez_et_al_2019" "Jaiswal_et_al" "Pan_et_al_2020" "Qian_et_al" "Turner_et_al_2022" "Vacante_et_al" "Wirka_et_al_2019" "Barcia_et_al_2024")
+# STARSOLO
+STUDIES=("Katyayani_et_al")
+
+# List of platform libraries corresponding to each study (in the same order)
+# choices=['10x','smartseq2','celseq2']
+# CELLRANGER
+# PLATFORM_LIBRARY="10x"
+# STARSOLO
+PLATFORM_LIBRARY="smartseq2"
 
 # Loop over studies and submit array jobs
 for STUDY in "${STUDIES[@]}"; do
@@ -34,7 +54,7 @@ for STUDY in "${STUDIES[@]}"; do
         fi
 
         # Submit SLURM job with correct array size
-        sbatch --array=0-$(($SAMPLE_COUNT - 1)) runPreprocess_rivanna.sh "$STUDY_PATH" "$STUDY"
+        sbatch --array=0-$(($SAMPLE_COUNT - 1)) runPreprocess_rivanna.sh --input-dir "$STUDY_PATH" --study "$STUDY" --platform "$PLATFORM_LIBRARY" --mpg "$MPG" --project-dir "$PROJECT_DIR" --generate-array --dry-run
     else
         echo "Warning: Study directory $STUDY_PATH does not exist. Skipping..."
     fi
